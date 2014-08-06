77015229bd077c39c3c7dd63e6a425e85c3c5c5b
Evan Lin <evanslin@gmail.com>
2014-08-07 00:06:23 +0800

    Add query criteria and refine default value

--- goMongoDB/main.go
+++ goMongoDB/main.go
@@ -15,6 +15,7 @@ type msg struct {
 type Book struct {
 	ISBN  string
 	TITLE string
+	PRICE int
 }
 
 type Person struct {
@@ -36,26 +37,33 @@ func main() {
 	c := session.DB("MongoTest1").C("books")
 
 	fmt.Println("Getting  Database count....")
-	count, err2 := c.Find(bson.M{} /*bson.M{"_id": 2}*/).Count() // .One(&result)
+	count, err2 := c.Find(bson.M{}).Count()
 	if err2 != nil {
 		panic(err)
 	}
 	fmt.Printf("total book count = %d\n", count)
 
 	if count == 0 {
-		err = c.Insert(&Book{"Ale1", "+55 53 8116 9639"})
-		err = c.Insert(&Book{"Ale2", "+55 53 8116 9639"})
-		err = c.Insert(&Book{"Ale3", "+55 53 8116 9639"})
+		err = c.Insert(&Book{"Ale1", "Book1", 35})
+		err = c.Insert(&Book{"Ale2", "Book2", 20})
+		err = c.Insert(&Book{"Ale3", "Book3", 40})
+		err = c.Insert(&Book{"Ale4", "Book4", 15})
+		err = c.Insert(&Book{"Ale5", "Book5", 55})
+		err = c.Insert(&Book{"Ale6", "Book6", 45})
 	}
-	err = c.Insert(&Book{"Ale4", "+55 53 8116 9639"})
-	err = c.Insert(&Book{"Ale5", "+55 53 8116 9639"})
-	err = c.Insert(&Book{"Ale6", "+55 53 8116 9639"})
 
 	result := Book{}
 	fmt.Println("Getting  data....")
-	err2 = c.Find(bson.M{"isbn": "Ale3"}).One(&result)
+	// Find book which prices is greater than(gt) 40
+	iter := c.Find(bson.M{"price": bson.M{"$gt": 40}}).Iter()
+	var index = 1
+
+	for iter.Next(&result) {
+		fmt.Printf("current result is [%d] result =%+v\n", index, result)
+		index++
+	}
 	//when search the DB it must all lower-case to avoid any error.
-	if err2 != nil {
+	if err2 := iter.Close(); err2 != nil {
 		fmt.Printf("No data\n")
 	} else {
 		fmt.Printf("result =%+v\n", result)
