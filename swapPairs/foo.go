48fb115bb800f4715d0487d58885d1007f1afcc0
Evan Lin <evanslin@gmail.com>
2021-09-24 10:52:18 +0800

    test case for recursion loop

---
+++ swapPairs/foo.go
@@ -0,0 +1,13 @@
+package foo
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func swapPairs(head *ListNode) *ListNode {
+
+	return nil
+}
+
+//https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681/
