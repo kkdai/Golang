4439af28f049f355d06ef5385d85ed5d92f6dad1
Evan Lin <evanslin@gmail.com>
2021-09-24 17:06:24 +0800

    u

--- swapPairs/foo.go
+++ swapPairs/foo.go
@@ -6,8 +6,20 @@ type ListNode struct {
 }
 
 func swapPairs(head *ListNode) *ListNode {
+	root := &ListNode{Next: head}
+	for prev, node := root, root.Next; node != nil; prev, node = node, node.Next {
+		if node.Next == nil {
+			break
+		}
+		swap(prev, node, node.Next)
+	}
+	return root.Next
+}
 
-	return nil
+func swap(pre, node, next *ListNode) {
+	node.Next = next.Next
+	next.Next = node
+	pre.Next = next
 }
 
 //https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681/
