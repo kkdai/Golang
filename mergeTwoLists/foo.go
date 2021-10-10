c977d05a2e2deaec5d8b936125f307afa6bfd942
Evan Lin <evan.if.lin@linecorp.com>
2021-10-10 22:31:17 +0800

    u

---
+++ mergeTwoLists/foo.go
@@ -0,0 +1,23 @@
+package foo
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+
+}
+
+//https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681/
