4bd1c754e47e7bad68f5405179b3e3fba5dc6664
Evan Lin <evan.if.lin@linecorp.com>
2021-10-10 22:43:31 +0800

    p

--- mergeTwoLists/foo.go
+++ mergeTwoLists/foo.go
@@ -12,7 +12,20 @@ type ListNode struct {
 }
 
 func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	if l1 == nil {
+		return l2
+	}
+	if l2 == nil {
+		return l1
+	}
+
+	if l1.Val < l2.Val {
+		l1.Next = mergeTwoLists(l1.Next, l2)
+		return l1
+	} else {
+		l2.Next = mergeTwoLists(l1, l2.Next)
+		return l2
+	}
 }
 
 //https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1681/
